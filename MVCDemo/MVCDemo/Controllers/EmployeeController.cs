291af6cd524e2d7b340d1425d724146e5bc97817
shaifulullah <shaiful.ullah@yahoo.com>
2018-02-01 23:08:01 -0500

    Connection to DB and get data from there

---
+++ MVCDemo/MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeId == id);
+
+            return View(employee);
+        }
+    }
+}
